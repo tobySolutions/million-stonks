a7034e45718f1ef87fbd7ffe0da3814844ca4b48
tobySolutions <adedejitobiloba7@gmail.com>
2023-07-14 09:20:49 +0100

    first push

---
+++ src/MillionStockTable.jsx
@@ -0,0 +1,47 @@
+import {block, For} from 'million/react';
+
+const StockRow = block(({name, symbol, price, delta}) => {
+    return (
+        <tr>
+            <td className="px-3 py-0 border-b border-gray-200 bg-white text-xs">
+                <p>
+                    {name} ({symbol}) is at ${price}
+                </p>
+                <div 
+                 style={{width: `${Math.abs(delta)}px`, height: '10px'}}
+                 className={delta >= 0 ? 'bg-green-500' : 'bg-red-500'} 
+                />
+            </td>
+        </tr>
+    )
+});
+
+const StockTable = block(({data})  => {
+    return (
+        <table className="border border-gray-200">
+            <thead>
+                <tr>
+                    <th className="px-3 py-0 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Market
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <For each={data} memo>
+                 {({name, symbol, price, delta}) => (
+                    <StockRow
+                      key={name + symbol}
+                      name={name}
+                      symbol={symbol}
+                      price={price}
+                      delta={delta}
+                    />
+                 )}
+                </For>
+            </tbody>
+        </table>
+    )
+});
+
+export default StockTable;
---
+++ src/ReactStockTable.jsx
@@ -0,0 +1,43 @@
+
+function StockRow ({name, symbol, price, delta}) {
+    return (
+        <tr>
+            <td className="px-3 py-0 border-b border-gray-200 bg-white text-xs">
+                <p>
+                    {name} ({symbol}) is at ${price}
+                </p>
+                <div 
+                 style={{width: `${Math.abs(delta)}px`, height: '10px'}}
+                 className={delta >= 0 ? 'bg-green-500' : 'bg-red-500'} 
+                />
+            </td>
+        </tr>
+    )
+};
+
+function StockTable ({data}) {
+    return (
+        <table className="border border-gray-200">
+            <thead>
+                <tr>
+                    <th className="px-3 py-0 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Market
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(({name, symbol, price, delta}) => (
+                    <StockRow
+                      key={name + symbol}
+                      name={name}
+                      symbol={symbol}
+                      price={price}
+                      delta={delta}
+                    />
+                ))}
+            </tbody>
+        </table>
+    )
+};
+
+export default StockTable;
